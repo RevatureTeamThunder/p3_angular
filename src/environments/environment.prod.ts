d1a7cce8f256748b2fc32573d38d091480bce11d
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-10-04 17:24:56 -0400

    Buildspec/Environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://ec2-3-92-223-165.compute-1.amazonaws.com:8080/",
+  baseUrl: "http://ec2-3-92-223-165.compute-1.amazonaws.com:8080",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://p3client-env.eba-ff9ffbt9.us-east-1.elasticbeanstalk.com/',
+    'Access-Control-Allow-Origin': 'http://p3-client.s3-website-us-east-1.amazonaws.com',
   },
 };
