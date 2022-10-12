005cf43339e487cfb641c385c7fac55ebaf8546a
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-10-12 10:52:21 -0400

    Registration email already in use handling

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
+
 
   registerForm = new UntypedFormGroup({
     fname: new UntypedFormControl(''),
@@ -17,18 +21,36 @@ export class RegisterComponent implements OnInit {
     password: new UntypedFormControl('')
   })
   
-
-  constructor(private authService: AuthService, private router: Router) { }
+  public showEmailError: Boolean
+  constructor(private authService: AuthService, private router: Router, ) { this.showEmailError = false }
 
   ngOnInit(): void {
+
+  }
+  
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      if (error.status === 500)
+      console.error(
+        
+        `Backend returned code ${error.status}, body was: `, error.error);
+        this.showEmailError=true;
+    }
+    // Return an observable with a user-facing error message.
+ 
   }
   
   onSubmit(): void {
+    
     this.authService.register(this.registerForm.get('fname')?.value, this.registerForm.get('lname')?.value, this.registerForm.get('email')?.value, this.registerForm.get('password')?.value).subscribe(
       () => console.log("New user registered"),
-      (err) => console.log(err),
+      (err) => this.handleError(err),
       () => this.router.navigate(['login'])
     );
-  }
 
+ 
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  
 
   authUrl: string = `${environment.baseUrl}/auth`;
   loggedIn: boolean = false;
@@ -24,7 +27,24 @@ export class AuthService {
   }
 
   register(firstName: string, lastName: string, email: string, password: string): Observable<any> {
-    const payload = {firstName: firstName, lastName: lastName, email: email, password: password};
-    return this.http.post<any>(`${this.authUrl}/register`, payload, {headers: environment.headers});
+    const payload = {firstName: firstName, lastName: lastName, email: email, password: password, role: 'user'};
+    // let params = new HttpParams();
+    // params = params.append("email", email);
+    // params = params.append("password", password);
+    // params = params.append("firstName", firstName);
+    // params = params.append("lastName", lastName);
+    // params = params.append("role", "user");
+    return this.http.post<any>(`${this.authUrl}/register`, payload,  {headers: environment.headers}).pipe(
+      catchError(error => {
+        if (error.status == 500)  {
+        throwError("Email already in use")
+        }
+        console.log("Registration error")
+        
+        return throwError(error);
+      })
+
+     
+    );
   }
 }
