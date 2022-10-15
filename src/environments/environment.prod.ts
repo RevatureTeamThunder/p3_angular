55cfc2048c59258f4e0e1892da0fc3e961646736
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-10-15 03:41:29 -0400

    Production environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,7 @@ export const environment = {
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://p3-client.s3-website-us-east-1.amazonaws.com',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Accept': 'application/json'
   },
 };
